ffaca9ca2012d4b0d87a9fd0656641dfdf9f6628
Eric Barb <eric.barb@gmail.com>
2018-09-12 13:08:08 -0400

    Setting up some auth stuff with Azure B2C

---
+++ src/app/auth/msal.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import * as Msal from 'msal';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MsalService {
+
+  access_token: any;
+  helper = new JwtHelperService();
+
+  clientApplication = new Msal.UserAgentApplication(environment.auth.clientID, environment.auth.authority,
+    (errorDesc: any, token: any, error: any, tokenType: any) => {
+      // Called after loginRedirect or acquireTokenPopup
+      this.saveAccessTokenToCache(token);
+    }
+  );
+
+  public login(): void {
+    this.clientApplication.loginRedirect(environment.auth.b2cScopes);
+  }
+
+  saveAccessTokenToCache(accessToken: string): void {
+    const profile = this.helper.decodeToken(accessToken);
+    sessionStorage.setItem('accessToken', accessToken);
+    sessionStorage.setItem('profile', JSON.stringify(profile));
+  }
+
+  logout(): void {
+    this.clientApplication.logout();
+  }
+
+  isOnline(): boolean {
+    return this.clientApplication.getUser() != null;
+  }
+
+}
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,28 +1,57 @@
-import { Component, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { Component, ChangeDetectorRef, OnDestroy, OnInit } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
+import { MsalService } from '../auth/msal.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent implements OnDestroy {
+export class LayoutComponent implements OnInit, OnDestroy {
 
   title = 'My CRM';
   mobileQuery: MediaQueryList;
+  profile: any;
   private _mobileQueryListener: () => void;
 
   constructor(
     changeDetectorRef: ChangeDetectorRef,
-    media: MediaMatcher
+    media: MediaMatcher,
+    private msalService: MsalService,
+    private location: Location
   ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
+  ngOnInit() {
+    if (sessionStorage.getItem('profile')) {
+      this.profile = JSON.parse(sessionStorage.getItem('profile'));
+    }
+  }
+
   ngOnDestroy() {
     this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  login(): void {
+    this.msalService.login();
+  }
+
+  logout(): void {
+    this.msalService.logout();
+    sessionStorage.clear();
+}
+
+  isActive(viewLocation: any): boolean {
+    return viewLocation === this.location.path();
+  }
+
+  isOnline(): boolean {
+      // console.log('Online Status: ', this.msalService.isOnline());
+      return this.msalService.isOnline();
   }
 
 }
